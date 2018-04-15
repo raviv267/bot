bbe9ede89e12564de1d37702da559c9890fe527f
raviv267 <36600744+raviv267@users.noreply.github.com>
2018-04-15 22:13:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'),description="Ghost's new Discord bot! \n\nHelp Commands:",owner_id=231028316843278346)
 
 @bot.command()
-async def testing():
+async def testing(ctx):
   await bot.say("testing")
 
 bot.run(os.environ.get('TOKEN'))
