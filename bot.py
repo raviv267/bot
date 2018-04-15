49749ec49feba015120ccfb7b017791e65080000
raviv267 <36600744+raviv267@users.noreply.github.com>
2018-04-15 21:46:55 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+import discord
+import sys
+import os
+from discord.ext import commands
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'),description="Ghost's new Discord bot! \n\nHelp Commands:",owner_id=231028316843278346)
+
+bot.run(os.environ.get('TOKEN'))
